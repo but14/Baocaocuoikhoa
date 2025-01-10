9d39e807002556d66884602ac7dd452fbe541b70
Longngoo <100961248+but14@users.noreply.github.com>
2025-01-10 18:36:06 +0700

    code

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame 
+import sys 
+
+pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Thu th■p bao lì xì")
+
+WHITE = (255, 255, 255)
+clock = pygame.time.Clock()
+FPS = 60
+
+def main():
+    running = True
+    while running:
+        clock.tick()
